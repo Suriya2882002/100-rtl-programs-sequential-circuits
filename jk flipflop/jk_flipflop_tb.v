69edfc33310cc826244b8238e10f1936bc480932
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-02-03 09:46:41 +0530

    Add files via upload

---
+++ jk flipflop/jk_flipflop_tb.v
@@ -0,0 +1,28 @@
+module jk_flipflop_tb;  
+   reg j;  
+   reg k;  
+   reg clk;  
+  
+   always #5 clk = ~clk;  
+  
+   jk_flipflop    jk0 ( .j(j),  
+                  .k(k),  
+                  .clk(clk),  
+                  .q(q));  
+  
+   initial begin  
+      j <= 0;  
+      k <= 0;  
+  
+      #5 j <= 0;  
+         k <= 1;  
+      #20 j <= 1;  
+          k <= 0;  
+      #20 j <= 1;  
+          k <= 1;  
+      #20 $finish;  
+   end  
+  
+   initial  
+      $monitor ("j=%0d k=%0d q=%0d", j, k, q);  
+endmodule  
