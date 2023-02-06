1bed665415fa0c6278e23458eb5b09b64f907041
Suriya2882002 <112601117+Suriya2882002@users.noreply.github.com>
2023-02-06 12:19:41 +0530

    Add files via upload

---
+++ mealy_1001_non/mealy_1001_tb.v
@@ -0,0 +1,29 @@
+module mealy_1001_tb;
+  reg clk, reset, din;
+  wire dout;
+  
+  mealy_1001 sd(clk, reset,din,dout);
+  initial clk = 0;   
+  always #2 clk = ~clk;
+    
+  initial begin
+    
+    din = 1;
+    #1 reset = 0;
+    #2 reset = 1;
+    
+    #3 din = 0;
+    #4 din = 0;
+    #4 din = 1;
+    #4 din= 1;
+    #4 din = 1;
+    #4 din = 0;
+    #4 din = 0;
+    #4 din = 1;
+    #4 din = 0;
+    #4 din = 1;
+    #4 din = 0;
+    #10;
+    $finish;
+  end
+endmodule
